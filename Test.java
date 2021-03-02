80f2383936dcaf250eaf64231f5ee50075835304
srijanbasu <srijan.basu@gmail.com>
2021-03-02 17:18:24 +0530

    Update and rename test.java to Test.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test
+{
+  public static void main(String [] args) throws Exception
+  {
+    System.out.println("Hello World");
+    System.out.println("First World");
+    System.out.println("Second Change");
+    System.out.println("Third Change");
+
+  }
+}
+
